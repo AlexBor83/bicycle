0bde0aca1079beb0020c7fceb6db4ea8448d7677
AlexBor83 <rabota_for_alex@mail.ru>
2021-08-26 23:31:50 +0400

    Поправил img

--- build/js/script.js
+++ build/js/script.js
@@ -1,28 +1,83 @@
-'use strict';
+"use strict";
 
-const headerNav = document.querySelector('.header__nav');
-    const headerToggle = document.querySelector('.header__toggle');
-    const body = document.querySelector('body');
-    const headerLinks = document.querySelectorAll('.header__link');
-    headerNav.classList.remove('header__nav--nojs');
+//Мобильное меню
+const headerNav = document.querySelector(".header__nav");
+const headerToggle = document.querySelector(".header__toggle");
+const body = document.querySelector("body");
+const headerLinks = document.querySelectorAll(".header__link");
+headerNav.classList.remove("header__nav--nojs");
 
-    headerToggle.addEventListener('click', () =>{
-      if (headerNav.classList.contains('header__nav--closed')) {
-      headerNav.classList.remove('header__nav--closed');
-      headerNav.classList.add('header__nav--opened');
-      body.classList.add('fixed-page');
-      } else {
-        headerNav.classList.remove('header__nav--opened');
-        body.classList.remove('fixed-page');
-        headerNav.classList.add('header__nav--closed');
-      }
-    } );
+headerToggle.addEventListener("click", () => {
+  if (headerNav.classList.contains("header__nav--closed")) {
+    headerNav.classList.remove("header__nav--closed");
+    headerNav.classList.add("header__nav--opened");
+    body.classList.add("fixed-page");
+  } else {
+    headerNav.classList.remove("header__nav--opened");
+    body.classList.remove("fixed-page");
+    headerNav.classList.add("header__nav--closed");
+  }
+});
 
-    headerLinks.forEach(item => {
-      item.addEventListener('click', () =>{
-        headerNav.classList.remove('header__nav--opened');
-        body.classList.remove('fixed-page');
-        headerNav.classList.add('header__nav--closed');
-      });
-    });
+headerLinks.forEach((item) => {
+  item.addEventListener("click", () => {
+    headerNav.classList.remove("header__nav--opened");
+    body.classList.remove("fixed-page");
+    headerNav.classList.add("header__nav--closed");
+  });
+});
 
+
+//Валидация формы
+const form = document.querySelector("form");
+const inputs = form.querySelectorAll("input");
+const inputTel = form.querySelector("input[name=tel]");
+const formButton = form.querySelector(".form__button");
+console.log(inputs[1]);
+
+form.noValidate = true;
+
+// запрет на ввод всего кроме цифр в поле с телефоном
+
+inputTel.addEventListener("input", ({ target }) => {
+  target.value = target.value.replace(/[^\d]/g, "");
+});
+
+// Отправка формы
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  console.log("Форма отправлена");
+
+  let error = formValidate(form);
+
+  if (error === 0) {
+    // Нужно доделать отправку формы на сервер
+    alert("Форма отправилась");
+  } else {
+    alert("Заполите обязательные поля");
+  }
+});
+
+function formValidate(form) {
+  let error = 0;
+
+  inputs.forEach((input) => {
+    formRemoveError(input);
+    if (input.value === "") {
+      formAddError(input);
+      error++;
+    }
+  });
+
+  return error;
+}
+
+function formAddError(input) {
+  input.classList.add("form__error");
+}
+
+function formRemoveError(input) {
+  input.classList.remove("form__error");
+}
